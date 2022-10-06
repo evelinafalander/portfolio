01f73ccff4025d52795f701f943f7704ef3ef680
evelinafalander <evel0838@stud.kea.dk>
2022-10-06 11:17:31 +0200

    hej

--- src/main.js
+++ src/main.js
@@ -17,3 +17,17 @@ function mouseOut() {
 /////Show info text
 
 
+var coll = document.getElementsByClassName("info");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
