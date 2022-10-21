44a7df4332e1d278431759cccfefc04a4290d99d
evelinafalander <evel0838@stud.kea.dk>
2022-10-21 22:22:12 +0200

    test-commit

--- src/main.js
+++ src/main.js
@@ -3,31 +3,26 @@ import "./style.css";
 import { animate, stagger, inView, scroll, timeline } from "motion";
 import { info } from "autoprefixer";
 
-document.getElementById("myMail").onmouseover = function() {mouseOver()};
-document.getElementById("myMail").onmouseout = function() {mouseOut()};
+document.getElementById("myMail").onmouseover = function () {
+  mouseOver();
+};
+document.getElementById("myMail").onmouseout = function () {
+  mouseOut();
+};
 
 function mouseOver() {
-  document.getElementById("myMail").classList.add('mymail');
+  document.getElementById("myMail").classList.add("mymail");
 }
 
 function mouseOut() {
-  document.getElementById("myMail").classList.remove('mymail');
+  document.getElementById("myMail").classList.remove("mymail");
 }
 
-/////Show info text
-
-
-var coll = document.getElementsByClassName("info");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
+//animations
+const items = document.querySelectorAll("#horisontalliste");
+scroll(
+  animate("#horisontalliste", {
+    transform: ["none", `translateX(-${items.length - 1}00vw)`],
+  }),
+  { target: document.querySelector("#horisontalscrollsection") }
+);
